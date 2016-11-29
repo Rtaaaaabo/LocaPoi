e238d165c0dbaae127e01ebd8b68ff5757be063f
Taku <r.taaaaabo@gmail.com>
2016-11-29 19:33:19 +0900

    お店情報をディレクトリーに集約

--- js/controller.js
+++ js/controller.js
@@ -1,8 +1,8 @@
 angular.module('myApp.controller', [])
 
 //★★★★★★★★★CurrentCtrl★★★★★★★★★★★★
-.controller('CurrentCtrl', function($scope, $http){
-  var api = 'http://api.gnavi.co.jp/RestSearchAPI/20150630/';   //レストランAPIのURL
+.controller('CurrentCtrl', function($scope, Current){
+  /*var api = 'http://api.gnavi.co.jp/RestSearchAPI/20150630/';   //レストランAPIのURL
   var keyId = '16066d25035a3a1853e0a5220e77ab00'; //アクセスキー
   //現在地から求めたいので仮で指定
   var latitude = 35.681298;
@@ -22,25 +22,33 @@ angular.module('myApp.controller', [])
     $http.jsonp(api, {params : params})
     .success(function(result) {
         $scope.items = result.rest;
-        console.log($scope.items);
     });
   }
   //APIを呼び出す
   $scope.callAPI();
+  //$scope.moredata = false;
+  $scope.items = [];
+  console.log($scope.items);*/
+  Current.getCurrent().then(function(items) {
+    $scope.items = items;
+  })
 
-  //$scope.items = Current.all();
-  /*$scope.moredata = false;
+  /*$scope.loadMoreData = function() {
+    console.log('loadMoreData');
+    Current.getNewCurrent().then(function(items){
+        $scope.items = $scope.items.concat(items);
+        $scope.$broadcast('scroll.infiniteScrollComplete');
+    })*/
 
-  $scope.loadMoreData = function() {
-    $scope.items.push({
+    /*$scope.items.push({
       id : $scope.items.length,
-      title : $scope.items.length,
-      distance : $scope.items.length
-    });
-    if($scope.items.length == 100){
+      name : $scope.items.name,
+      category : $scope.items.category,
+      budget : $scope.items.budget
+    });*/
+    /*if($scope.items.length == 100){
       $scope.moredata = true;
     }
-    $scope.$broadcast('scroll.infiniteScrollComplete');
   }*/
 })
 
--- js/service.js
+++ js/service.js
@@ -1,60 +1,35 @@
 angular.module('myApp.service',[])
 
-.factory('Current', function(){
+.factory('Current', function($http){
+  var items = [];
+  var api = 'http://api.gnavi.co.jp/RestSearchAPI/20150630/';   //レストランAPIのURL
+  var keyId = '16066d25035a3a1853e0a5220e77ab00'; //アクセスキー
+  //現在地から求めたいので仮で指定
+  var latitude = 35.681298;
+  var longitude = 139.766247;
+  var params = {
+    keyid : keyId,
+    latitude : latitude,
+    longitude : longitude,
+    range : 3,
+    callback : 'JSON_CALLBACK',
+    format : 'json'
+  };
 
-
-
-  /*var items = [
-   {
-      id:0,
-      title : '古川未鈴',
-      distance : 540,
-      guide : '日本の女性アイドル。でんぱ組.incのメンバーで、同グループではセンターを務める。香川県高松市出身。愛称はみりんちゃん。でんぱ組.incでのキャッチフレーズは「歌って踊れるゲーマーアイドル」、担当色は赤',
-      image : 'img/mirin.png'
-    },
-    {
-      id:1,
-      title : '相沢梨紗',
-      distance : 350,
-      guide : '日本の女性アイドル。でんぱ組.incのメンバーで、同グループではリーダーを務める。大阪府豊中市出身。愛称はりさちー。でんぱ組.incでのキャッチフレーズは「2.5次元伝説!」、担当色は白。',
-      image : 'img/risa.jpg'
-    },
-    {
-      id:2,
-      title : '夢眠ねむ',
-      distance : 250,
-      image : 'img/nemu.jpg'
-    },
-    {
-      id : 3,
-      title : '成瀬瑛美',
-      distance : 689,
-      image : 'img/eimi.jpg'
-    },
-    {
-      id : 4,
-      title : '最上もが',
-      distance : 567,
-      image : 'img/moga.jpg'
-    },
-    {
-      id : 5,
-      title : '藤咲彩音',
-      distance : 789,
-      image : 'img/ayane.jpg'
-    }
-  ];
   return {
-    all : function() {
-      return items;
+    getCurrent : function() {
+      return $http.jsonp(api, {params : params}).then(function(response){
+        items = response.data.rest;
+        console.log(items);
+        return items;
+      })
+
     },
-    get : function(currentId) {
-      for (var i = 0; i < items.length; i++) {
-        if(items[i].id === parseInt(currentId)){
-          return items[i];
-        }
-      }
-      return null;
-    }
-  };*/
+    /*getNewCurrent : function() {
+      return $http.jsonp(api, {params : params})
+      .sucess(function(result) {
+        items = result.rest;
+      });
+    }*/
+  }
 })
